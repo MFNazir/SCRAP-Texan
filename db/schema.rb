f8efa5eb26a0c5ba334f43d54571fadd5106478f
Jordan Medrano <jordanmedrano@gmail.com>
2020-03-31 03:58:54 -0500

    layout for home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_072120) do
+ActiveRecord::Schema.define(version: 2020_03_31_064051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,11 @@ ActiveRecord::Schema.define(version: 2020_03_26_072120) do
     t.index ["customer_status_id"], name: "index_customers_on_customer_status_id"
     t.index ["customer_type_id"], name: "index_customers_on_customer_type_id"
     t.index ["state_province_id"], name: "index_customers_on_state_province_id"
+  end
+
+  create_table "dashboard_calculations", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "employee_statuses", force: :cascade do |t|
