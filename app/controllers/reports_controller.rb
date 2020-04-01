d0d39f41af2517cd30f5247f8bc88061c7e97167
mfnazir <mnazir9876@gmail.com>
2020-03-31 20:43:31 -0500

    Made Employees show up on Reports page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,11 @@
 class ReportsController < ApplicationController
   def index
+      employee_status_id = EmployeeStatus.find_by(employee_status: "Active").id()
+      @employees = Employee.where(employee_status_id: employee_status_id)
+    end  
   end
-end
+
+
+
+
+
