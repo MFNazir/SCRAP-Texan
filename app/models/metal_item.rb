3a1e2c19806a309715c5044871c8a5c951c1fbea
mfnazir <mnazir9876@gmail.com>
2020-04-06 14:42:39 -0500

    fixed metal item price calculations

--- app/models/metal_item.rb
+++ app/models/metal_item.rb
@@ -5,9 +5,9 @@ class MetalItem < ApplicationRecord
   accepts_nested_attributes_for :invoices, reject_if: :all_blank, allow_destroy: true
 
 
-  validates :net_weight, :presence => true, numericality: {greater_than_or_equal_to: 0,  message: ":value cannot be negative"}
+  validates :net_weight, :presence => true, format: { with: /\A\d+(?:\.\d{1})?\z/ }, numericality: {greater_than_or_equal_to: 0,  message: ":value cannot be negative"}
 
-  validates :unit_price, :presence => true, numericality: {greater_than_or_equal_to: 0,  message: ":value cannot be negative"}
+  validates :unit_price, :presence => true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0, less_than: 1000000,  message: ":value cannot be negative"}
   
   validates :metal_item_desc, :metal_type_id, :presence => true
 
