357e897190a8f2a7d25484f97fe40d31e91847e6
mfnazir <mnazir9876@gmail.com>
2020-04-06 05:32:31 -0500

    final push before bed

--- app/models/metal_item.rb
+++ app/models/metal_item.rb
@@ -4,8 +4,11 @@ class MetalItem < ApplicationRecord
   has_many :invoices, through: :line_items
   accepts_nested_attributes_for :invoices, reject_if: :all_blank, allow_destroy: true
 
-  validates :total, :presence => true, length: { minimum: 1, maximum: 11 }
+  validates :total, :presence => true, length: { minimum: 1, maximum: 11 }, numericality: {greater_than_or_equal_to: 0,  message: ":cannot be negative"}
 
+  validates :net_weight, :presence => true, numericality: {greater_than_or_equal_to: 0,:allow_nil => true,  message: ":cannot be negative"}
 
-  validates :metal_item_desc, :net_weight, :unit_price, :metal_type_id, :presence => true
+  validates :unit_price, :presence => true, numericality: {greater_than_or_equal_to: 0,:allow_nil => true,  message: ":cannot be negative"}
+  
+  validates :metal_item_desc, :metal_type_id, :presence => true
 end
