dfdf4fc84e146f5373a1d83794497e44fb2ea7df
mfnazir <mnazir9876@gmail.com>
2020-04-06 07:25:17 -0500

    Fixed some stuff regarding net weight in invoices and metal items

--- app/models/metal_item.rb
+++ app/models/metal_item.rb
@@ -5,9 +5,9 @@ class MetalItem < ApplicationRecord
   accepts_nested_attributes_for :invoices, reject_if: :all_blank, allow_destroy: true
 
 
-  validates :net_weight, :presence => true, numericality: {greater_than_or_equal_to: 0,  message: ":cannot be negative"}
+  validates :net_weight, :presence => true, numericality: {greater_than_or_equal_to: 0,  message: ":value cannot be negative"}
 
-  validates :unit_price, :presence => true, numericality: {greater_than_or_equal_to: 0,  message: ":cannot be negative"}
+  validates :unit_price, :presence => true, numericality: {greater_than_or_equal_to: 0,  message: ":value cannot be negative"}
   
   validates :metal_item_desc, :metal_type_id, :presence => true
 
