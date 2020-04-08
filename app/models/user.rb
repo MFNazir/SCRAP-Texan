103e9eadaed7ff84c36a2648e17a500e99241b0b
mfnazir <mnazir9876@gmail.com>
2020-04-08 05:15:38 -0500

    added some sick password validation like a hackerman and added ** to forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
     # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
 
-    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 Uppercase, 1 Lowercase, 1 Digit and 1 Special Character'
   end
 end
