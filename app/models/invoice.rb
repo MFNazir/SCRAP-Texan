e5258b98d2d2e68e46ae2d3a25ce5f424606796f
mfnazir <mnazir9876@gmail.com>
2020-04-08 14:54:03 -0500

    added more validation for metal image

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -10,6 +10,7 @@ class Invoice < ApplicationRecord
   has_one_attached :metal_img
   validates :metal_items, :presence => true
   validates :invoice_number, :presence => true, length: { minimum: 7, maximum: 7 }
-  validates :invoice_date_time, :metal_img, :customer_id, :employee_id, :invoice_type_id, :invoice_status_id, :invoice_desc, :presence => true
+  validates :metal_img, blob: { content_type: :image }
+  validates :invoice_date_time, :customer_id, :employee_id, :invoice_type_id, :invoice_status_id, :invoice_desc, :presence => true
 
 end
