98b161d635fd2603b4dff8aaa41fa4244d94b131
mfnazir <mnazir9876@gmail.com>
2020-04-09 01:27:37 -0500

    dl image now validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,7 +13,7 @@ class Customer < ApplicationRecord
   validates :zip_code, :presence => true, length: { minimum: 5 }
   validates :dl_state, :presence => true, length: { minimum: 2, maximum: 2 }
   validates :cust_m_initial, length: { minimum: 2, maximum: 2 }, allow_blank: true
-  validates :dl_image, blob: { content_type: :image }
+  validates :dl_image, :presence => true, blob: { content_type: :image }
   validates :cust_email, :presence => true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :cust_f_name, :cust_l_name, :cust_address, :cust_city, :dl_number, :dob, :state_province_id, :country_id, :customer_status_id, :customer_type_id, :presence => true
   validates :customer_vehicles, :presence => true
